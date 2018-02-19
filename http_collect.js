0061b96fb8131bd254d62bb6aeebfc36a595220a
Bauke Posthuma <baukeposthuma@gmail.com>
2018-02-19 23:23:19 +0100

    Added code for HTTP collect and concat-stream module

---
+++ http_collect.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const concat = require('concat-stream')
+
+
+
+http.get(process.argv[2], function(response) {
+    response.setEncoding('utf8')
+    response.on('error', function(err){
+        return console.log(err)
+        console.log(err)
+    })
+    var concatBuffer = concat(function(buffer){
+        console.log(buffer.length)
+        console.log(buffer)
+    })
+    response.pipe(concatBuffer)
+})
