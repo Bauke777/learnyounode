e0098c41665ddd5b72b4e07e5f98ab430b021c13
Bauke Posthuma <baukeposthuma@gmail.com>
2018-02-19 23:23:51 +0100

    Updated code of HTTP client
    Made the code more understandable, but less pretty

--- http_client.js
+++ http_client.js
@@ -3,6 +3,15 @@ const [, , url] = process.argv
 
 http.get(url, function (response) {
     response.setEncoding('utf8')
-    response.on('data', console.log)
-    response.on('error', console.error)
-}).on('error', console.error)
+    response.on('error', function(err){
+        console.log(err)
+    })
+    response.on('data', function(data){
+        console.log(data)
+    })
+})
+
+
+// Eventueel zou dit netter kunnen zijn:
+// response.on('data', console.log)
+// response.on('error', console.error)
